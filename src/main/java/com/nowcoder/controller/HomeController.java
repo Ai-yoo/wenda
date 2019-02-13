d08aba6987d58204cf9c6757a138b28cb800e71a
Ai-yoo <duzhentong123@foxmail.com>
2019-02-13 12:14:01 +0800

    增加分页

--- src/main/java/com/nowcoder/controller/HomeController.java
+++ src/main/java/com/nowcoder/controller/HomeController.java
@@ -1,23 +1,24 @@
 package com.nowcoder.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.nowcoder.aspect.LogAspect;
 import com.nowcoder.model.*;
 import com.nowcoder.service.CommentService;
 import com.nowcoder.service.FollowService;
 import com.nowcoder.service.QuestionService;
 import com.nowcoder.service.UserService;
+import com.nowcoder.util.PageUtil;
+import org.apache.http.HttpRequest;
 import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -68,13 +69,47 @@ public class HomeController {
     }
 
     @RequestMapping(path = {"/", "/index"}, method = {RequestMethod.GET})
-    public String index(Model model) {
-        model.addAttribute("vos", getQuestions(0, 0, 10));
+    public String index(@RequestParam(required = false, defaultValue = "1", value = "currentPage") Integer currentPage,
+                        @RequestParam(required = false, defaultValue = "0", value = "userId") Integer userId,
+                        Model model, HttpServletRequest request) {
+        PageUtil page = splitPage();
+        //总页数
+        int totalPage = page.getTotalPage();
+        model.addAttribute("totalPage", totalPage);
+
+        //总条数
+        int totalQuestion = questionService.getQuestionNum();
+        model.addAttribute("totalQuestionNum", totalQuestion);
+
+        //当前页
+        if (currentPage <= 0) {
+            currentPage = 1;
+        } else if (currentPage > totalPage) {
+            currentPage = totalPage;
+        }
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("p", currentPage);
+
+        int per = currentPage - 2;
+        int next = currentPage + 2;
+        if (per <= 0) {
+            per = 1;
+        } else if (next > totalPage) {
+            next = totalPage;
+        }
+        List<Integer> pageList = new ArrayList();
+        for (int i = per; i <= next; i++) {
+            pageList.add(i);
+        }
+        model.addAttribute("pageList", pageList);
+
+        model.addAttribute("url", request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort());
+        model.addAttribute("vos", getQuestions(userId, currentPage, page.getPageSize()));
         return "index";
     }
 
     private List<ViewObject> getQuestions(int userId, int offset, int limit) {
-        List<Question> questionList = questionService.getLatestQuestion(userId, offset, limit);
+        List<Question> questionList = questionService.getLatestQuestion(userId, offset,limit);
         List<ViewObject> vos = new ArrayList<>();
         for (Question question : questionList) {
             ViewObject vo = new ViewObject();
@@ -83,6 +118,27 @@ public class HomeController {
             vos.add(vo);
         }
         return vos;
+    }
+
+//    private List<ViewObject> getQuestions() {
+//        List<Question> questionList = questionService.selectQuestions();
+//        List<ViewObject> vos = new ArrayList<>();
+//        for (Question question : questionList) {
+//            ViewObject vo = new ViewObject();
+//            vo.set("question", question);
+//            vo.set("user", userService.getUser(question.getUserId()));
+//            vos.add(vo);
+//        }
+//        return vos;
+//    }
+
+
+    private PageUtil splitPage() {
+        PageUtil page = new PageUtil();
+        int totalQuestion = questionService.getQuestionNum();
+        int pageSize = page.getPageSize();
+        page.setTotalPage((totalQuestion % pageSize == 0) ? totalQuestion / pageSize : totalQuestion / pageSize + 1);
+        return page;
     }
 
 }
